640c83c3864cb39368cb203f24cc64ac0b18fcc9
DJABHipHop <anacastellano@bellsouth.net>
2016-10-21 16:04:53 -0400

    Update TinyURLAPI.swift
    add Url Valdation

--- TinyURLAPI.swift
+++ TinyURLAPI.swift
@@ -9,17 +9,30 @@
 import UIKit
 extension UIViewController {
     func ShortenURL(URLToSorten:String) -> String {
-        guard let apiEndpoint = URL(string: "http://tinyurl.com/api-create.php?url=\(URLToSorten)")else {
-            self.ErrorMessage(error:("Error: doesn't seem to be a valid URL") as String)
+        if verifyUrl(urlString:URLToSorten) != false{
+            guard let apiEndpoint = URL(string: "http://tinyurl.com/api-create.php?url=\(URLToSorten)")else {
+                self.ErrorMessage(error:("Error: doesn't seem to be a valid URL") as String)
+                return "" as String
+            }
+            do {
+                let shortURL = try String(contentsOf: apiEndpoint, encoding: String.Encoding.ascii)
+                return shortURL as String
+            } catch let Error{
+                self.ErrorMessage(error:Error.localizedDescription)
+                return URLToSorten as String
+            }
+        }else{
+            self.ErrorMessage(error:"\(URLToSorten) doesn't seem to be a valid URL or is blank")
             return "" as String
         }
-        do {
-            let shortURL = try String(contentsOf: apiEndpoint, encoding: String.Encoding.ascii)
-            return shortURL as String
-        } catch let Error{
-            self.ErrorMessage(error:Error.localizedDescription)
-            return URLToSorten as String
+    }
+    func verifyUrl (urlString: String?) -> Bool {
+        if let urlString = urlString {
+            if let url  = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
         }
+        return false
     }
     func ErrorMessage(error:String) {
         let ErrorMessageAlert = UIAlertController(title:"Error", message: error, preferredStyle: UIAlertControllerStyle.alert)
