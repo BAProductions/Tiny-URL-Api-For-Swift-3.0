42737c0d85ac4683a2479a7fc48b24ee25d70f35
DJABHipHop <anacastellano@bellsouth.net>
2016-10-22 15:38:42 -0400

    Update TinyURLAPI.swift

--- TinyURLAPI.swift
+++ TinyURLAPI.swift
@@ -3,8 +3,20 @@
 //  Press Themes Portfolio
 //
 //  Created by BAproductions on 10/21/16.
-//  Copyright © 2016 letsbuildthatapp. All rights reserved.
-//
+// Copyright (C) <2016>  <DJABHipHop/BAProductions>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import UIKit
 extension UIViewController {
